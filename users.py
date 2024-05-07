39397530d70ff22a143f58854c55536bcc092c43
Amir Damavandi <amirromanista2004@gmail.com>
2024-05-07 14:59:12 +0330

    create User class

---
+++ users.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, username, password, phone_number, id, birthdate, date_joined):
+        self.username = username
+        self.password = password
+        self.phone_number = phone_number
+        self.id = id
+        self.birthdate = birthdate
+        self.birthdate = birthdate
+        self.date_joined = date_joined
