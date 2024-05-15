06a3e331e2684157a8d74b50ff3231ca68082a42
Amir Damavandi <amirromanista2004@gmail.com>
2024-05-15 19:48:00 +0330

    update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from users import User
-import hashlib
-import getpass
-import re
 
 
 def logged_in():
     user = User(None, None, None, None, None, None)
     while True:
         login_status = input('press 2 to login, 1 for signup, to close this press 0: ')
-        hashing = hashlib.new('SHA256')
-        hashing.update(str(user.password).encode())
-        user_information = {'username': user.username, 'password': hashing.hexdigest(),
-                            'phone_number': user.phone_number, 'id': user.id,
-                            'birthdate': user.birthdate, 'date_joined': user.date_joined
-                            }
         if login_status == '1':
             sign_up = user.sign_up()
             print(sign_up)
