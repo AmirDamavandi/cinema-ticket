d1d1396b19e329fd6749d86e121f9a167fa370f0
Amir Damavandi <amirromanista2004@gmail.com>
2024-05-16 13:43:58 +0330

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from users import User
 
 
 def logged_in():
-    user = User(None, None, None, None, None, None)
+    user = User(None, None, None, None, None, None, None)
     while True:
         login_status = input('press 2 to login, 1 for signup, to close this press 0: ')
         if login_status == '1':
