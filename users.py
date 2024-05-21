9f022de6820f5cb374090a93e8ac2fd165b2a2d8
Amir Damavandi <amirromanista2004@gmail.com>
2024-05-21 15:28:38 +0330

    login method added for class Admin

--- users.py
+++ users.py
@@ -76,7 +76,7 @@ class User(AbstractUser):
                     else:
                         return 'incorrect phone number'
                 else:
-                    return 'incorrect password'
+                    return 'incorrect or weak password'
             else:
                 return 'this username already exist'
         else:
@@ -387,14 +387,13 @@ class User(AbstractUser):
         return legged_in()
 
 
-class Admin(AbstractUser, ABC):
+class Admin(AbstractUser):
     def sign_up(self):
         username = input('enter your username: ')
         username_pattern = r'^[a-zA-Z0-9_.]+$'
         if re.match(username_pattern, username):
-            unique_username = os.listdir('users_information')
-            unique_admin = os.listdir('admins_information')
-            if not username.lower() in unique_username and username.lower() in unique_admin:
+            unique_username = os.listdir('admins_information')
+            if not username.lower() in unique_username:
                 password = getpass.getpass('enter your password: ')
                 password_pattern = r'^[a-zA-Z0-9!@#$%&*()_+=\'\-.]{8,}$'
                 if re.match(password_pattern, password):
@@ -413,7 +412,7 @@ class Admin(AbstractUser, ABC):
                     else:
                         return 'incorrect phone number'
                 else:
-                    return 'incorrect password'
+                    return 'incorrect or weak password'
             else:
                 return 'this username already exist'
         else:
@@ -437,8 +436,165 @@ class Admin(AbstractUser, ABC):
                 'plans': self.plans
 
             }
-            with open(f'admins_information/{username.lower()}', 'w+', encoding='utf-8') as information:
-                json.dump(users_information, information)
-            information.close()
-            return 'your account has been created successfully'
+            with open(f'admins_information/{username.lower()}', 'w+', encoding='utf-8') as admin_information:
+                json.dump(users_information, admin_information)
+            admin_information.close()
+            return 'Admin account has been created successfully'
 
+    def login(self):
+        username = input('enter your username: ').lower()
+        password = getpass.getpass('enter your password: ')
+        user_exist = os.listdir('admins_information')
+
+        def legged_in():
+            logged_in = False
+            try:
+                with open(f'admins_information/{username}', 'r', encoding='utf-8') as login_information:
+                    matching_information = json.load(login_information)
+                if username == str(matching_information['username']).lower() and password == matching_information[
+                    'password']:
+                    logged_in = True
+                user_information = {
+                    'username': matching_information['username'],
+                    'password': matching_information['password'],
+                    'phone_number': matching_information['phone_number'],
+                    'id': matching_information['id'],
+                    'birthdate': matching_information['birthdate'],
+                    'date_joined': matching_information['date_joined'],
+                    'plans': matching_information['plans']
+                }
+                if logged_in:
+                    print('logged in successfully!')
+                    while True:
+                        options = input('security and privacy: 1, to get a plan 2 and 0 to log out: ')
+                        if options == '1':
+                            while True:
+                                security_options = input('to check your information press 1, to change information '
+                                                         'press 2, press 0 to back: ')
+                                if security_options == '1':
+                                    hash_pass = hashlib.sha256(matching_information['password'].encode('utf-8'))
+                                    print(f'username: {matching_information['username']}'
+                                          f'\npassword: {hash_pass.hexdigest()}'
+                                          f'\nphone number: {matching_information['phone_number']}'
+                                          f'\nid: {matching_information['id']}'
+                                          f'\nbirthdate: {matching_information['birthdate']}'
+                                          f'\ndate joined: {matching_information['date_joined']}'
+                                          f'\nplans: {matching_information['plans']}')
+                                elif security_options == '2':
+                                    while True:
+                                        information_changing = input(
+                                            'to change your username 1, to change phone number '
+                                            '2, to change password 3, press 0 to back: ')
+                                        if information_changing == '1':
+                                            while True:
+                                                new_username = input(
+                                                    f'your current username "{matching_information['username']}" '
+                                                    f'enter new one, press 0 to'
+                                                    f' cancel it: ')
+                                                username_changed = False
+                                                if new_username == '0':
+                                                    break
+                                                username_pattern = r'^[a-zA-Z0-9_.]+$'
+                                                unique = os.listdir('users_information')
+                                                username_in_list = new_username.lower() in unique
+                                                if (re.match(username_pattern, new_username)
+                                                        and new_username.lower() != str(
+                                                            matching_information['username']).lower()
+                                                        and not username_in_list):
+                                                    user_information['username'] = new_username
+                                                    os.chdir('users_information')
+                                                    os.renames(matching_information['username'], new_username.lower())
+                                                    os.chdir('..')
+                                                    with open(f'admins_information/{new_username}', 'w',
+                                                              encoding='utf-8') as username_changing:
+                                                        json.dump(user_information, username_changing)
+                                                    username_changing.close()
+                                                    matching_information = user_information
+                                                    username_changed = True
+                                                    print('your username has been changed successfully')
+                                                    break
+                                                elif not re.match(username_pattern, new_username):
+                                                    print(f'"{new_username}" isn\'t a correct username')
+                                                elif new_username.lower() == str(
+                                                        matching_information['username']).lower():
+                                                    print(f'"{new_username}" is current username')
+                                                elif username_in_list:
+                                                    print(f'"{new_username}" has already taken')
+
+                                        elif information_changing == '2':
+                                            while True:
+                                                new_phone_number = input(
+                                                    f'your current "{matching_information['phone_number']}", enter new '
+                                                    f'one, press 0 to cancel it: ')
+                                                phone_number_changed = False
+                                                if new_phone_number == '0':
+                                                    break
+                                                if matching_information['phone_number'] is not None:
+                                                    if new_phone_number == matching_information['phone_number']:
+                                                        print(f'"{new_phone_number}" is current phone number')
+                                                if (new_phone_number.isdigit() and len(new_phone_number) == 11
+                                                        and new_phone_number != matching_information['phone_number']
+                                                ):
+                                                    user_information['phone_number'] = new_phone_number
+                                                    with open(f'admins_information/{matching_information['username']}',
+                                                              'w',
+                                                              encoding='utf-8') as phone_number_changing:
+                                                        json.dump(user_information, phone_number_changing)
+                                                    phone_number_changing.close()
+                                                    matching_information = user_information
+                                                    phone_number_changed = True
+                                                    print('your phone number has been changed successfully')
+                                                    break
+                                                else:
+                                                    if new_phone_number != matching_information['phone_number']:
+                                                        print(f'"{new_phone_number}" isn\'t a correct phone number')
+                                        elif information_changing == '3':
+                                            while True:
+                                                changed_password = False
+                                                current_password = getpass.getpass(
+                                                    'enter your current password, press 0 '
+                                                    'to cancel: ')
+                                                if current_password == '0':
+                                                    break
+                                                if current_password == matching_information['password']:
+                                                    password_pattern = r'^[a-zA-Z0-9!@#$%&*()_+=\'\-.]{8,200}$'
+                                                    new_password = getpass.getpass('enter your new password: ')
+                                                    if re.match(password_pattern, new_password):
+                                                        if new_password != matching_information['password']:
+                                                            confirm_password = getpass.getpass(
+                                                                'confirm your password: ')
+                                                            if confirm_password == new_password:
+                                                                user_information['password'] = confirm_password
+                                                                with open(
+                                                                        f'admins_information/{matching_information[
+                                                                            'username']}',
+                                                                        'w',
+                                                                        encoding='utf-8') as password_changing:
+                                                                    json.dump(user_information, password_changing)
+                                                                password_changing.close()
+                                                                matching_information = user_information
+                                                                changed_password = True
+                                                                print('your password has changed successfully')
+                                                                break
+                                                            else:
+                                                                print('password does not match')
+                                                        else:
+                                                            print('it\'s your current password')
+                                                    else:
+                                                        print('password is incorrect')
+                                                else:
+                                                    print('password is wrong')
+                                        elif information_changing == '0':
+                                            break
+                                elif security_options == '0':
+                                    break
+                        elif options == '2':
+                            pass
+                        elif options == '0':
+                            break
+                else:
+                    print('your password does not match')
+            except FileNotFoundError:
+                print('there\'s no user with this information')
+
+        return legged_in()
