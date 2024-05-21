b74b63538eb273d80e0090d8b368f66bfb95740e
Amir Damavandi <amirromanista2004@gmail.com>
2024-05-21 15:26:56 +0330

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from users import User
+from users import User, Admin
 
 
 def logged_in():
     user = User(None, None, None, None, None, None, None)
+    admin = Admin(None, None, None, None, None, None, None)
     while True:
         login_status = input('press 2 to login, 1 for signup, to close this press 0: ')
         if login_status == '1':
             print(user.sign_up())
         elif login_status == '2':
             user.login()
+        elif login_status == 'Admin'.lower():
+            while True:
+                admin_login_status = input('(Admin), press 2 to login, 1 for signup, to close this press 0: ')
+                if admin_login_status == '1':
+                    print(admin.sign_up())
+                elif admin_login_status == '2':
+                    admin.login()
+                elif admin_login_status == '0':
+                    break
         elif login_status == '0':
             break
 
